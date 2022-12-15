0175ecc5fb26cc3a03be65d17346ecfa7f8b48ce
Neven Đuranec <neven.duranec@gmail.com>
2022-12-15 09:25:57 +0100

    WIP

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import './style.css';
+import data from './data.json';
+const mainNav = document.querySelector('#main-navigation');
+const template = '<li data-{PLANET}><a href="{HREF}">{LABEL}</a></li>';
+let out = '';
+
+for (const planet of data) {
+    out += template
+        .replace('{LABEL}', planet.name)
+        .replace('{HREF}', `#${planet.name.toLowerCase()}`)
+        .replace('{PLANET}', planet.name.toLowerCase());
+}
+
+mainNav.innerHTML = out;
+
+const links = mainNav.querySelectorAll('a');
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        console.log(
+            data.filter(
+                (planet) =>
+                    planet.name.toLowerCase() ===
+                    link.getAttribute('href').slice(1)
+            )
+        );
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{html,js}",
-        "./index.html"
-    ],
+    content: ['./src/**/*.{html,js}', './index.html'],
     theme: {
         colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            "cyan": "hsl(158, 36%, 37%)",
-            "cyan-dark": "hsl(158, 36%, 15%)",
-            "cream": "hsl(30, 38%, 92%)",
-            "blue": "hsl(212, 21%, 14%)",
-            "blue-gray": "hsl(228, 12%, 48%)",
-            "white": "hsl(0, 0%, 100%)"
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: 'hsl(0, 0%, 100%)',
+            mercury: 'hsl(194, 48%, 49%)',
+            venus: 'hsl(33, 82%, 61%)',
+            earth: 'hsl(263, 67%, 51%)',
+            mars: 'hsl(10, 63%, 51%)',
+            jupiter: 'hsl(2, 68%, 53%)',
+            saturn: 'hsl(17, 73%, 46%)',
+            uranus: 'hsl(169, 73%, 44%)',
+            neptune: 'hsl(222, 87%, 56%)',
+            gray: 'hsl(240, 17%, 26%)',
+            grayLite: 'hsl(240, 6%, 54%)',
+            dark: 'hsl(240, 67%, 8%)',
         },
         fontFamily: {
-            'montserrat': ["'Montserrat'", "sans-serif"],
-            'fraunces': ["'Fraunces'", "serif"]
+            antonio: ["'Antonio'", 'sans-serif'],
+            spartan: ["'Spartan'", 'sans-serif'],
         },
         screens: {
-            'main': '686px',
+            main: '686px',
         },
     },
-    plugins: []
+    plugins: [],
 };
